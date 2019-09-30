befe86609e7c4b308add53072dcd950ea761ca3d
Ryan <ryanzhao@live.com>
2019-09-29 20:03:48 -0700

    Initial working copy of StreamChart

--- src/DataFetch.js
+++ src/DataFetch.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import StreamChart from './StreamChart';
 
 
-const API_SERVER_HOST = process.env.REACT_APP_API_SERVER_HOST || "https://cors-anywhere.herokuapp.com/http://ufo-api.herokuapp.com/api/sightings/search?limit=100";
+const API_SERVER_HOST = process.env.REACT_APP_API_SERVER_HOST || "https://cors-anywhere.herokuapp.com/http://ufo-api.herokuapp.com/api/sightings/search?limit=20000";
 
 class DataFetch extends Component {
 	constructor() {
@@ -15,9 +15,10 @@ class DataFetch extends Component {
     }
 
   componentDidMount() {
+    var referenceToOriginalThis = this
     axios.get(API_SERVER_HOST)
       .then(function (response) {
-        console.log(response);
+        referenceToOriginalThis.setState( {ufoData: response.data.sightings} )
       })
       .catch(function (error) {
         console.log(error);
@@ -25,9 +26,10 @@ class DataFetch extends Component {
   }
 
   render() {
+      // console.log(this.state.ufoData)
       return (
     	    <div>
-            {/* <StreamChart data={this.state.ufoData} /> */}
+            <StreamChart data={this.state.ufoData} />
 	        </div>
       );
   }
